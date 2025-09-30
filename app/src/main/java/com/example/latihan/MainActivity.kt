5d5641e4e41d85ef80b6c6c16c6549a7d4a18b2c
Devin AF <devinayersfunata@gmail.com>
2025-09-30 19:48:19 +0700

    Main Activiy

--- app/src/main/java/com/example/latihan/MainActivity.kt
+++ app/src/main/java/com/example/latihan/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.latihan
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var txAngka1: EditText
+    private lateinit var txAngka2: EditText
+    private lateinit var btnTambah: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        txAngka1 = findViewById(R.id.txAngka1)
+        txAngka2 = findViewById(R.id.txAngka2)
+        btnTambah = findViewById(R.id.btnTambah)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, Fragment1())
+            .commit()
+
+        btnTambah.setOnClickListener {
+            val angka1 = txAngka1.text.toString().toIntOrNull() ?: 0
+            val angka2 = txAngka2.text.toString().toIntOrNull() ?: 0
+            val hasil = angka1 + angka2
+
+            val fragment2 = Fragment2.newInstance("$angka1 + $angka2 = $hasil")
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment2)
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
